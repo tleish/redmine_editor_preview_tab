02bb9132cabee73b42576575f18fe4a74968f8c0
Thomas Leishman <thomas.leishman@plansource.com>
2016-02-06 09:32:29 -0700

    Adding German locale

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_editor_preview_tab do
   name 'Redmine Editor Preview Tab Extension'
   author 'Thomas Leishman'
   description 'The Redmine Editor Preview Tab Extension adds a preview tab to the Redmine text editor, similar to the editor on Github.'
-  version '0.1.3'
+  version '0.1.4'
   url 'https://github.com/tleish/redmine_editor_preview_tab'
   author_url 'https://github.com/tleish'
   settings :default => { hide_default_preview: false, enabled: true } , :partial => 'redmine_editor_preview_tab/settings'
